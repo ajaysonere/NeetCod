51d17a8f6a9656aef2da643c9cbdf7daf45c79cb
Ajay Sonere <ajaysonere786@gmail.com>
2023-07-05 11:06:50 +0530

    duplicate present

--- 01. Array and Hashing/Easy/containsDuplicate.cpp
+++ 01. Array and Hashing/Easy/containsDuplicate.cpp
@@ -1,8 +1,20 @@
 #include<iostream>
+#include<vector>
+#include<unordered_set>
 using namespace std;
+
+bool containsDuplicate(vector<int>vec){
+    unordered_set<int> st(vec.begin() , vec.end());
+    return st.size()!= vec.size();
+}
 
 int main ()
 {
-
+    vector<int> vec = {10,20,30,40,50 , 50};
+    if(containsDuplicate(vec)){
+        cout<< "Duplicate Present " << "\n";
+    }else{
+        cout << "Duplicate not Present " << "\n";
+    }
     return 0;
 }
