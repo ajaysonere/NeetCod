fc521a302b5fb5d16138dffdaf1b7c8a3f6a20f3
Ajay Sonere <ajaysonere786@gmail.com>
2023-07-06 22:44:08 +0530

    concatination

--- 01. Array and Hashing/Easy/concatinationOfArray.cpp
+++ 01. Array and Hashing/Easy/concatinationOfArray.cpp
@@ -1,7 +1,21 @@
 #include<iostream>
+#include<vector>
 using namespace std;
+
+vector<int> concatinationOfArray(vector<int> vec){
+    vector<int> ans = vec;
+    for(auto x : vec){
+        ans.push_back(x);
+    }
+    return ans;
+}
 
 int main(){
     
+    vector<int> vec = {1,2,3,4};
+    vector<int> ans = concatinationOfArray(vec);
+    for(int i=0; i<ans.size(); i++){
+        cout << ans[i] << " ";
+    }
     return 0;
 }
