61365b56b9aaaa31776805b0c420d761998b9c5a
Ajay Sonere <ajaysonere786@gmail.com>
2023-07-07 16:50:05 +0530

    is Subsquence

--- 01. Array and Hashing/Easy/isSubsquence.cpp
+++ 01. Array and Hashing/Easy/isSubsquence.cpp
@@ -1,7 +1,28 @@
 #include<iostream>
+#include<stack>
 using namespace std;
+
+bool isSubsquenceUsingStack(string s, string t){
+    stack<char> st;
+    for(int i=0; i<s.size(); i++){
+        st.push(s[i]);
+    }
+    for(int j=t.size()-1; j>=0; j--){
+        if(st.top() == t[j]){
+            st.pop();
+        }
+    }
+    return st.empty();
+}
 
 int main ()
 {
+    string s = "abc";
+    string t = "avdbdfc";
+    if(isSubsquenceUsingStack(s,t)){
+        cout << "It is subsquence " << "\n";
+    }else{
+        cout << "It is not subsquence " << "\n";
+    }
     return 0;
 }
