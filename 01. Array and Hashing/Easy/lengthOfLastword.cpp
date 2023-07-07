01a608660219d8f4188c8faf52e3e4b43199d5cb
Ajay Sonere <ajaysonere786@gmail.com>
2023-07-07 17:07:00 +0530

    length of last world

--- 01. Array and Hashing/Easy/lengthOfLastword.cpp
+++ 01. Array and Hashing/Easy/lengthOfLastword.cpp
@@ -1,8 +1,19 @@
 #include<iostream>
 using namespace std;
 
+int getLength(string str){
+    int count = 0;
+    for(int i= str.size()-1; i>=0; i--){
+        if(str[i] != ' ')count++;
+        if(str[i] == ' ' && count > 0) break;
+    }
+    return count;
+}
+
 int main ()
 {
-
-return 0;
+    string str = "my name is ajay sonere  ";
+    int lengthOfLastWord = getLength(str);
+    cout << "Length Of Last Word " << lengthOfLastWord << "\n";
+    return 0;
 }
