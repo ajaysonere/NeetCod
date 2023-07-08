9dd9b1024b36ebfb41e93b88e2e68159bc948780
Ajay Sonere <ajaysonere786@gmail.com>
2023-07-08 19:20:42 +0530

    two sum

---
+++ 01. Array and Hashing/Easy/twoSum.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+pair<int,int> twoSum(vector<int> sum , int target){
+    
+}
+
+int main ()
+{
+    vector<int> sum = {2,7,11,15};
+    int target = 9;
+    pair<int,int> ans = twoSum(sum , target);
+    cout << ans.first << " " << ans.second << "\n";
+    return 0;
+}
