62530d34a9b642b87d1b47a6d05dc3b9768bc148
Ajay Sonere <ajaysonere786@gmail.com>
2023-07-08 21:31:03 +0530

    longest common prefix

--- 01. Array and Hashing/Easy/longestCommonPrefix.cpp
+++ 01. Array and Hashing/Easy/longestCommonPrefix.cpp
@@ -1,7 +1,29 @@
 #include<iostream>
+#include<vector>
+#include<algorithm>
 using namespace std;
+string longestCommonPrefix(vector<string> strs){
+    string ans = "";
+    sort(strs.begin(), strs.end());
+    int size = strs.size();
+    string first = strs[0];
+    string last = strs[size - 1];
+
+    for (int i = 0; i < first.size(); i++)
+    {
+        if (first[i] != last[i])
+        {
+            break;
+        }
+        ans += first[i];
+    }
+    return ans;
+}
 
 int main ()
 {
+    vector<string> strs = {"flower","flow","flight"};
+    string ans = longestCommonPrefix(strs);
+    cout << "Longest Common Prefix is : " << ans << "\n";
     return 0;
 }
