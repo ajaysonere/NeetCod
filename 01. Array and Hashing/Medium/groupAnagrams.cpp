416be577a38c1d815e9975b90012d1107c25d8bd
Ajay Sonere <ajaysonere786@gmail.com>
2023-07-09 21:41:08 +0530

    group anagrams

--- 01. Array and Hashing/Medium/groupAnagrams.cpp
+++ 01. Array and Hashing/Medium/groupAnagrams.cpp
@@ -1,15 +1,35 @@
 #include<iostream>
 #include<vector>
 #include<unordered_map>
+#include<algorithm>
 using namespace std;
 
-vector<vector<string>> groupAnagrams(String &str){
-    
+vector<vector<string>> groupAnagrams(vector<string> str)
+{
+    vector<vector<string>> ans;
+    unordered_map<string , vector<string>>mp;
+    for(auto it : str){
+        string temp = it;
+        sort(it.begin() , it.end());
+        mp[it].push_back(temp);
+    }
+
+    for(auto it : mp){
+        vector<string> t = it.second;
+        ans.push_back(t);
+    }
+    return ans;
 }
 
 int main ()
 {
     vector<string> str = {"eat","tea","tan","ate","nat","bat"};
     vector<vector<string>> ans = groupAnagrams(str);
+    for(int i=0; i<ans.size(); i++){
+        for(int j=0; j<ans[i].size(); j++){
+            cout << ans[i][j] << " ";
+        }
+        cout << "\n";
+    }
     return 0;
 }
