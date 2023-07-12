1ccab1acd0037687b297a15aa4128c8813d9ed2a
Ajay Sonere <ajaysonere786@gmail.com>
2023-07-12 21:13:16 +0530

    revrese string

--- 01. Array and Hashing/Easy/reverseString.cpp
+++ 01. Array and Hashing/Easy/reverseString.cpp
@@ -1,8 +1,18 @@
 #include<iostream>
 using namespace std;
 
+void reverseString(string &s){
+    int start = 0;
+    int end = s.size()-1;
+    while(start <end){
+        swap(s[start++] , s[end--]);
+    }
+}
+
 int main ()
 {
-
+    string str = "hello";
+    reverseString(str);
+    cout << str << "\n";
     return 0;
 }
