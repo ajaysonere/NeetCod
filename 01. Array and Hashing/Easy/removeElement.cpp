efe1b2ea437703ba0ac456d40fdf0bd9d6b919b8
Ajay Sonere <ajaysonere786@gmail.com>
2023-07-14 22:51:20 +0530

    brute force for remove element

--- 01. Array and Hashing/Easy/removeElement.cpp
+++ 01. Array and Hashing/Easy/removeElement.cpp
@@ -1,7 +1,28 @@
 #include<iostream>
+#include<vector>
 using namespace std;
+
+int removeElement(vector<int>nums , int val){
+    vector<int> temp;
+    for(int i=0; i<nums.size(); i++){
+        if(nums[i] != val){
+            temp.push_back(nums[i]);
+        }
+    }
+    nums = temp;
+    for(auto x: nums){
+        cout << x << " ";
+    }
+
+    return temp.size();
+}
+
 
 int main ()
 {
+    vector<int> nums = {0,1,2,2,3,0,4,2};
+    int val = 2;
+    int ans = removeElement(nums , val);
+    cout << ans << "\n";
     return 0;
 }
