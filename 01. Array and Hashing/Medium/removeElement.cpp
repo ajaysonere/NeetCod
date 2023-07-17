fa1275c6bbecf6f567a781e1d993cfe041913e44
Ajay Sonere <89720260+ajaysonere@users.noreply.github.com>
2023-07-17 23:32:52 +0530

    Remove element using set

--- 01. Array and Hashing/Medium/removeElement.cpp
+++ 01. Array and Hashing/Medium/removeElement.cpp
@@ -1,6 +1,13 @@
 #include<iostream>
 using namespace std;
-
+vector<int> removeElementUsingSet(vector<int>nums){
+  set<int> st(nums.begin() , nums.end());
+  vector<int>duplicate;
+  for(auto x : duplicate){
+    duplicate.push_back(x);
+  }
+  return duplicate;
+}
 int main(){
     cout << "Remove Element" << "\n";
     return 0;
