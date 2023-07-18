2a979042b54a41111a841eae19cb1f54377b89db
Ajay Sonere <ajaysonere786@gmail.com>
2023-07-18 22:17:55 +0530

    unique emails

--- 01. Array and Hashing/Easy/uniqueEmailAddress.cpp
+++ 01. Array and Hashing/Easy/uniqueEmailAddress.cpp
@@ -1,7 +1,37 @@
 #include<iostream>
+#include<vector>
+#include<set>
 using namespace std;
+
+void isEmail(string str, set<string> &s)
+{
+    string local = "";
+    for (int i = 0; i < str.size() && str[i] != '@'; i++)
+    {
+        if (str[i] == '+')
+            break;
+        if (str[i] == '.')
+            continue;
+        local += str[i];
+    }
+    local += str.substr(str.find('@'));
+    s.insert(local);
+}
+
+int numUniqueEmails(vector<string> &emails)
+{
+    set<string> s;
+    for (int i = 0; i < emails.size(); i++)
+    {
+        isEmail(emails[i], s);
+    }
+    return s.size();
+}
 
 int main ()
 {
+    vector<string> emails = {"a@leetcode.com","b@leetcode.com","c@leetcode.com"};
+    int ans = numUniqueEmails(emails);
+    cout << ans << "\n";
     return 0;
 }
