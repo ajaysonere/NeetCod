c1a6f07647c5cb5012eb4c8d4434cde2618e40e4
Ajay Sonere <ajaysonere786@gmail.com>
2023-07-18 22:29:32 +0530

    isomorphic string

--- 01. Array and Hashing/Easy/isomorphicsString.cpp
+++ 01. Array and Hashing/Easy/isomorphicsString.cpp
@@ -1,7 +1,29 @@
 #include<iostream>
+#include<vector>
+#include<unordered_map>
 using namespace std;
+
+ bool isIsomorphic(string s, string t) {
+        unordered_map<char , vector<int>> mp1;
+        unordered_map<char , vector<int>> mp2;
+        for(int i=0; i<s.size(); i++){
+            mp1[s[i]].push_back(i);
+            mp2[t[i]].push_back(i);
+            if(mp1[s[i]] != mp2[t[i]]){
+                return false;
+            }
+        }
+        return true;
+    }
 
 int main ()
 {
-    return 0;
+        string s = "paper";
+        string t = "title";
+        if(isIsomorphic(s,t)){
+            cout<< "True" << "\n";
+        }else{
+            cout << "False" << "\n";
+        }
+        return 0;
 }
