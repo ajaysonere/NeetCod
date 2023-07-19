f501d5b90ce3bdaa160cd9b8ecc77b8c91614113
Ajay Sonere <ajaysonere786@gmail.com>
2023-07-19 23:37:05 +0530

    remove element

--- 02. Two Pointer/Easy/removeElement.cpp
+++ 02. Two Pointer/Easy/removeElement.cpp
@@ -1,4 +1,6 @@
 #include<iostream>
+#include<vector>
+#include<set>
 using namespace std;
 vector<int> removeElementUsingSet(vector<int>nums){
   set<int> st(nums.begin() , nums.end());
@@ -9,15 +11,23 @@ vector<int> removeElementUsingSet(vector<int>nums){
   return duplicate;
 }
 
-vector<int>removeElementUsingVec(vector<int>nums){
-    
+int removeElementUsingVec(vector<int>&nums){
+    int element = -2147484;
+    int index = 0;
+    for(int i=0; i<nums.size(); i++){
+        if(nums[i] != element){
+          element = nums[i];
+          nums[index++] = element;
+        }
+    }
+    return index;
 }
 
 int main(){
     vector<int> nums={1,1,2,5,5,5,10,10,20};
-    vector<int>ans = removeElementUsingSet(nums);
-    for(auto x : ans){
-      cout << ans << endl;
+    int index = removeElementUsingVec(nums);
+    for(int i=0; i<index; i++){
+        cout << nums[i] << " ";
     }
     return 0;
 }
