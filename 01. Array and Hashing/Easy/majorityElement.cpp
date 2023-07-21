79a411923014f7085d80966ea263ef44862dbe76
Ajay Sonere <ajaysonere786@gmail.com>
2023-07-21 19:00:27 +0530

    majority element

--- 01. Array and Hashing/Easy/majorityElement.cpp
+++ 01. Array and Hashing/Easy/majorityElement.cpp
@@ -1,7 +1,22 @@
 #include<iostream>
+#include<vector>
+#include<unordered_map>
 using namespace std;
+
+int majorityElementUsingMap(vector<int> nums){
+    unordered_map<int,int>mp;
+    for(auto x : nums){
+        if(++mp[x] > nums.size()/2){
+            return x;
+        }
+    }
+    return 0;
+}
 
 int main ()
 {
+    vector<int> nums = {2,2,1,1,1,2,2};
+    int ans = majorityElementUsingMap(nums);
+    cout << ans << "\n";
     return 0;
 }
