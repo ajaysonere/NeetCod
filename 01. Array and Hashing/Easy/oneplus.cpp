959a1feeb0136b986d4aaeea10931901c6d2c3f8
Ajay Sonere <ajaysonere786@gmail.com>
2023-07-21 18:40:47 +0530

    onePlus

--- 01. Array and Hashing/Easy/oneplus.cpp
+++ 01. Array and Hashing/Easy/oneplus.cpp
@@ -1,7 +1,30 @@
 #include<iostream>
+#include<vector>
+
 using namespace std;
 
+vector<int> onePlus(vector<int> nums){
+    int last = nums.size()-1;
+    while(last >= 0){
+        if(nums[last] == 9){
+            nums[last] = 0;
+        }else{
+            nums[last]++;
+            return nums;
+        }
+        last--;
+    }
+    nums.insert(nums.begin(),1);
+    return nums;
+}
+
 int main(){
-  cout << "One Plus " << "\n";
+   // cout << "One Plus " << "\n";
+   vector<int> nums = {1,2,3};
+   vector<int> ans = onePlus(nums);
+   for(auto x : ans){
+      cout << x << " ";
+   }
+
   return 0;
 }
