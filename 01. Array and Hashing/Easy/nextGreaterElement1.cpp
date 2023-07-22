d7feff5d0e981be09cadb56ae919be207a249621
Ajay Sonere <ajaysonere786@gmail.com>
2023-07-22 23:15:57 +0530

    find pivot element

--- 01. Array and Hashing/Easy/nextGreaterElement1.cpp
+++ 01. Array and Hashing/Easy/nextGreaterElement1.cpp
@@ -1,7 +1,19 @@
 #include<iostream>
+#include<vector>
 using namespace std;
+
+vector<int> nextGreaterElement(vector<int> nums1 , vector<int>nums2){
+    
+}
+
 
 int main ()
 {
+    vector<int> nums1 = {4, 1, 2};
+    vector<int> nums1 = {1, 3, 4, 2};
+    vector<int> ans = nextGreaterElement(nums1 , nums2);
+    for(auto x : ans){
+        cout << x << " ";
+    }
     return 0;
 }
