0d4865270e1647f841d6d21bcc88b4a00acfb7d9
Ajay Sonere <ajaysonere786@gmail.com>
2023-07-24 21:45:34 +0530

    find all numbers disApperred

--- 01. Array and Hashing/Easy/findAllNumbersDisAppered.cpp
+++ 01. Array and Hashing/Easy/findAllNumbersDisAppered.cpp
@@ -1,7 +1,28 @@
 #include<iostream>
+#include<vector>
+#include<unordered_set>
 using namespace std;
+
+vector<int>  findAllNumbersDisAppered(vector<int>&nums){
+    unordered_set<int> st(nums.begin() , nums.end());
+    vector<int> ans;
+    int disNumber = 1;
+    for(int i=0; i<nums.size(); i++){
+        if(st.count(disNumber) == 0){
+           ans.push_back(disNumber);
+        }
+        disNumber++;
+    }
+    return ans;
+}
 
 int main ()
 {
+    vector<int> nums = {4,3,2,7,8,2,3,1};
+    vector<int> ans = findAllNumbersDisAppered(nums);
+
+    for(auto x : ans){
+      cout << x << " ";
+    }
     return 0;
 }
