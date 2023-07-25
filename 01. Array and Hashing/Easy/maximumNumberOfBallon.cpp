1ff48a99da23f664001f016d4afd90fb93828c49
Ajay Sonere <ajaysonere786@gmail.com>
2023-07-25 23:08:18 +0530

    maximum number of ballon

--- 01. Array and Hashing/Easy/maximumNumberOfBallon.cpp
+++ 01. Array and Hashing/Easy/maximumNumberOfBallon.cpp
@@ -1,7 +1,31 @@
 #include<iostream>
+#include<vector>
+#include<limits.h>
+#include<algorithm>
 using namespace std;
+
+ int maxNumberOfBalloons(string text) {
+        vector<int> freq(26 , 0);
+        for(int i=0; i<text.size(); i++){
+            int k = text[i]-'a';
+            freq[k]++;
+        }
+        string str = "balloon";
+        int ans = INT_MAX;
+        for(auto x : str){
+            if(x == 'l' || x == 'o'){
+                ans = min(ans , freq[x-'a']/2);
+            }else{
+                ans = min(ans , freq[x-'a']);
+            }
+        }        
+       return ans;
+    }
 
 int main ()
 {
+    string s = "loonbalxballpoon";
+    int numberOfTimes = maxNumberOfBalloons(s);
+    cout << "Maximum number of balloons : " << numberOfTimes << "\n";
     return 0;
 }
