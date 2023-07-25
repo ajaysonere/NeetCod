c3806413757eab7090509a73ff4ea7194a8702bc
Ajay Sonere <ajaysonere786@gmail.com>
2023-07-25 23:23:59 +0530

    design hashset

--- 01. Array and Hashing/Easy/designHashset.cpp
+++ 01. Array and Hashing/Easy/designHashset.cpp
@@ -1,5 +1,43 @@
 #include<iostream>
+#include<vector>
 using namespace std;
+
+class MyHashSet {
+public:
+    vector<int>Hashset; 
+    MyHashSet() {
+       
+    }
+    
+    void add(int key) {
+        if(contains(key)){
+           return ;
+        }else{
+           Hashset.push_back(key);
+        }
+    }
+    
+    void remove(int key) {
+        int index = -1;
+        for(int i=0; i<Hashset.size(); i++){
+            if(Hashset[i] == key){
+                index = i;
+            }
+        }
+        if(index != -1){
+            Hashset.erase(Hashset.begin()+index);
+        }
+    }
+    
+    bool contains(int key) {
+        for(int i=0; i<Hashset.size(); i++){
+            if(Hashset[i] == key){ 
+                return true;
+            }
+        }
+        return false;
+    }
+};
 
 int main ()
 {
