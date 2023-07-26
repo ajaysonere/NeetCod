d705871b1cd599eafd0d4cf262866f75b19d807c
Ajay Sonere <ajaysonere786@gmail.com>
2023-07-26 23:30:21 +0530

    sort an array

--- 01. Array and Hashing/Medium/sortAnArray.cpp
+++ 01. Array and Hashing/Medium/sortAnArray.cpp
@@ -1,7 +1,25 @@
 #include<iostream>
+#include<vector>
 using namespace std;
+
+vector<int> sortArrayByNaive(vector<int> nums){
+
+    for(int i=0;i<nums.size()-1; i++){
+        for(int j=0; j<nums.size()-i-1; j++){
+            if(nums[j] > nums[j+1]){
+               swap(nums[j] , nums[j+1]);
+            }
+        }
+    }
+    return nums;
+}
 
 int main ()
 {
+    vector<int> nums = {1,20,3,5,4,65,32,45,10};
+    vector<int> ans = sortArrayByNaive(nums);
+    for(auto x : ans)
+       cout << x << " ";
+    
     return 0;
 }
