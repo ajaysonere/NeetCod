17185c9ef4ac4c0918f22f9f05e9673aa64fe071
Ajay Sonere <ajaysonere786@gmail.com>
2023-07-26 23:25:47 +0530

    design an hashmap

--- 01. Array and Hashing/Easy/designHashMap.cpp
+++ 01. Array and Hashing/Easy/designHashMap.cpp
@@ -1,6 +1,26 @@
 #include<iostream>
 using namespace std;
 
+class MyHashMap {
+public:
+    int arr[1000001];
+    MyHashMap() {
+        fill(arr,arr+1000000 , -1);
+    }
+    
+    void put(int key, int value) {
+        arr[key] = value;
+    }
+    
+    int get(int key) {
+        return arr[key];
+    }
+    
+    void remove(int key) {
+        arr[key] = -1;
+    }
+};
+
 int main ()
 {
     return 0;
