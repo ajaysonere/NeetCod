06c8c0a35b74222768f2dcc70fd00d2e6415ce0d
Ajay Sonere <ajaysonere786@gmail.com>
2023-07-27 23:22:43 +0530

    using the map in c++

--- 01. Array and Hashing/Medium/topKFrequentElement.cpp
+++ 01. Array and Hashing/Medium/topKFrequentElement.cpp
@@ -1,7 +1,23 @@
 #include<iostream>
+#include<vector>
+#include<map>
 using namespace std;
+
+vector<int> topKFrequentElement(vector<int> nums , int k){
+     map<int , int> mp;
+     for(auto x : nums){
+        mp[x]++;
+     }
+     
+}
 
 int main ()
 {
+    vector<int> nums = {1,1,1,2,2,3};
+    int k = 2;
+    vector<int> ans = topKFrequentElement(nums , k);
+    for(auto x : ans){
+        cout << x << " ";
+    }
     return 0;
 }
