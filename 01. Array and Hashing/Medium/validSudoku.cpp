7d892f3dcb522fcbf1edab2c6e7610080f984e74
Ajay Sonere <ajaysonere786@gmail.com>
2023-07-28 22:12:37 +0530

    valid sudoku

--- 01. Array and Hashing/Medium/validSudoku.cpp
+++ 01. Array and Hashing/Medium/validSudoku.cpp
@@ -1,5 +1,37 @@
 #include<iostream>
+#include<vector>
 using namespace std;
+
+bool validSudoku(vector<vector<char>> &board){
+    set<string>st;
+        for(int i=0; i<9; i++){
+            for(int j=0; j<9; j++){
+                if(board[i][j] != '.'){
+                    string ro = to_string(i);
+                    string co = to_string(j);
+                    string tempBoard = to_string(board[i][j]);
+                    string matrix = to_string((i/3)*3+(j/3));
+                    string forRow = "Row"+ro+tempBoard;
+                    string forCol = "Col"+co+tempBoard;
+                    string forBox = "Box"+matrix+tempBoard;
+                    if(st.find(forRow) == st.end()){
+                        st.insert(forRow);
+                    }else if(st.find(forCol) == st.end()){
+                        st.insert(forCol);
+                    }
+                    else{
+                        return false;
+                    }
+                    if(st.find(forBox) == st.end()){
+                        st.insert(forBox);
+                    }else{
+                        return false;
+                    }
+                }
+            }
+        }
+        return true;
+}
 
 int main ()
 {
