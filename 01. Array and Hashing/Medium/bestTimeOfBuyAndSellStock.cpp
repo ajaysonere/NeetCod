9bf2379cfdbcb3f0dbe9031a49850f1e68816ea7
Ajay Sonere <ajaysonere786@gmail.com>
2023-07-30 22:00:46 +0530

    best time to buy and sell stock

--- 01. Array and Hashing/Medium/bestTimeOfBuyAndSellStock.cpp
+++ 01. Array and Hashing/Medium/bestTimeOfBuyAndSellStock.cpp
@@ -1,7 +1,21 @@
 #include<iostream>
+#include<vector>
 using namespace std;
+
+int buyAndSellStock(vector<int> prices){
+    int sum = 0;
+        for(int i=1; i<prices.size(); i++){
+            if(prices[i] > prices[i-1]){
+                sum += prices[i]-prices[i-1];
+            }
+        }
+        return sum;
+}
 
 int main ()
 {
+    vector<int> prices = {7,1,5,3,6,4};
+    int ans = buyAndSellStock(prices);
+    cout << ans << "\n";
     return 0;
 }
