f1668caf58c1226bcaa24d53ff0d790340edf317
Ajay Sonere <ajaysonere786@gmail.com>
2023-07-31 23:14:00 +0530

    minimun difference between highest and lowest k

--- 02. Two Pointer/Easy/minimumDifferenceBetweenHighAndLow.cpp
+++ 02. Two Pointer/Easy/minimumDifferenceBetweenHighAndLow.cpp
@@ -1,7 +1,25 @@
 #include<iostream>
+#include<vector>
+#include<climits>
+#include<algorithm>
 using namespace std;
+
+int minimumDifference(vector<int>& nums, int k) {
+        sort(nums.begin() , nums.end());
+        int i = 0;
+        int j = k-1;
+        int ans = INT_MAX;
+        while(j<nums.size()){
+            ans = min(ans , nums[j++] - nums[i++]);
+        }
+        return ans;
+    }
 
 int main ()
 {
-    return 0;
+        vector<int> nums = {9, 4, 1, 7};
+        int k = 2;
+        int ans = minimumDifference(nums , k);
+        cout << ans << "\n";
+        return 0;
 }
