12868433bc846c35530e39eddf8c80c20514cbc3
Ajay Sonere <ajaysonere786@gmail.com>
2023-07-31 23:05:50 +0530

    valid palindrome II

--- 02. Two Pointer/Easy/validPalindromeII.cpp
+++ 02. Two Pointer/Easy/validPalindromeII.cpp
@@ -1,7 +1,35 @@
 #include<iostream>
 using namespace std;
 
+ bool helper(string s , int start , int end){
+        while(start < end){
+            if(s[start] != s[end]) return false;
+            start++;
+            end--;
+        }
+        return true;
+    }
+    
+    bool validPalindrome(string s) {
+        int start = 0;
+        int end  = s.size()-1;
+        while(start < end){
+            if(s[start] != s[end]){
+               return helper(s , start+1, end) || helper(s , start ,end-1);
+            }
+            start++;
+            end--;
+        }
+        return true;
+    }
+
 int main ()
 {
+    string str = "aba";
+    if(validPalindrome(str)){
+        cout << "Palindrome " << "\n";
+    }else{
+        cout << "Not Palindrome " << "\n";
+    }
     return 0;
 }
