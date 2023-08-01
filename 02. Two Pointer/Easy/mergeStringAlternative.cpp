82a4532e3579cdbdd4a5ca9a3ccd91dbb0800d00
Ajay Sonere <ajaysonere786@gmail.com>
2023-08-01 23:27:29 +0530

    merge string alternative

--- 02. Two Pointer/Easy/mergeStringAlternative.cpp
+++ 02. Two Pointer/Easy/mergeStringAlternative.cpp
@@ -1,7 +1,27 @@
 #include<iostream>
 using namespace std;
 
+string mergeAlternately(string word1, string word2) {
+        string temp;
+        int i=0 , j=0;
+        while(i < word1.size() && j < word2.size()){
+            temp += word1[i++];
+            temp += word2[j++];
+        }
+        while(i<word1.size()){
+            temp +=word1[i++];
+        }
+        while(j<word2.size()){
+            temp += word2[j++];
+        }
+        return temp;
+}
+
 int main ()
 {
+    string str1 = "abc";
+    string str2 = "xyz";
+    string ans = mergeAlternately(str1 , str2);
+    cout << ans << "\n";
     return 0;
 }
