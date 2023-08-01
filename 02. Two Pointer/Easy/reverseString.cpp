9f5a72112ecf9ccf7b39f9309ec08884b6062fc4
Ajay Sonere <ajaysonere786@gmail.com>
2023-08-01 23:35:41 +0530

    reverse string

--- 02. Two Pointer/Easy/reverseString.cpp
+++ 02. Two Pointer/Easy/reverseString.cpp
@@ -1,7 +1,22 @@
 #include<iostream>
+#include<algorithm>
+#include<vector>
 using namespace std;
+
+ void reverseString(vector<char>& s) {
+        int start = 0;
+        int end = s.size()-1;
+        while(start < end){
+            swap(s[start++] , s[end--]);
+        }
+}
 
 int main ()
 {
+    vector<char> str = {'h' , 'e' , 'l', 'l' , 'o'};
+    reverseString(str);
+    for(auto x : str){
+        cout << x << " ";
+    }
     return 0;
 }
