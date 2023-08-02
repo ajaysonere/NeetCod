4f07b73ab854900a46162eb1a5cf5ba82f010251
Ajay Sonere <ajaysonere786@gmail.com>
2023-08-02 23:10:44 +0530

    moveZeros to end

--- 02. Two Pointer/Easy/moveZeros.cpp
+++ 02. Two Pointer/Easy/moveZeros.cpp
@@ -3,18 +3,21 @@
 using namespace std;
 
 void moveZeroes(vector<int>& nums) {
-        // vector<int> ans;
-        // int count =0;
-        // for(auto x : nums){
-        //     if(x == 0) count++;
-        //     else ans.push_back(x);
-        // }
-        // while(count >0 ){
-        //     ans.push_back(0);
-        //     count--;
-        // }
-        // nums = ans;
-        int index =0;
+        vector<int> ans;
+        int count =0;
+        for(auto x : nums){
+            if(x == 0) count++;
+            else ans.push_back(x);
+        }
+        while(count >0 ){
+            ans.push_back(0);
+            count--;
+        }
+        nums = ans;
+}
+
+void moveZerosOptimized(vector<int> &nums){
+     int index =0;
         for(int i=1; i<nums.size(); i++){
             if(nums[index] == 0 && nums[i] != 0){
                 swap(nums[index] , nums[i]);
@@ -28,12 +31,10 @@ void moveZeroes(vector<int>& nums) {
 }
 
 
-
-
 int main ()
 {
     vector<int> nums = {0,1,0,3,12};
-    moveZeroes(nums);
+    moveZerosOptimized(nums);
     for(auto x : nums){
         cout << x << " ";
     }
