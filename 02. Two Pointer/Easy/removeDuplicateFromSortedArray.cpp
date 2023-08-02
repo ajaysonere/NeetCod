1f043c6eb2fb8bc6cafa2fc8551a139c9ca3a009
Ajay Sonere <ajaysonere786@gmail.com>
2023-08-02 23:17:00 +0530

    remove duplicate

--- 02. Two Pointer/Easy/removeDuplicateFromSortedArray.cpp
+++ 02. Two Pointer/Easy/removeDuplicateFromSortedArray.cpp
@@ -1,7 +1,27 @@
 #include<iostream>
+#include<vector>
+#include<set>
 using namespace std;
+
+int removeDuplicates(vector<int>& nums) {
+        set<int> s;
+        for(auto x: nums){
+            s.insert(x);
+        }
+        int index = 0;
+        for(auto it = s.begin(); it != s.end(); it++ ){
+            nums[index++] = *it;
+        }
+        return s.size();
+}
+
 
 int main ()
 {
+    vector<int> nums = {0,0,1,1,1,2,2,3,3,4};
+    int ansIndex = removeDuplicates(nums);
+    for(int i=0; i<ansIndex; i++){
+        cout << nums[i] << " ";
+    }
     return 0;
 }
