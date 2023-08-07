7fbd4a8d9c93c7e5102fd799960b8f10949dccf0
Ajay Sonere <ajaysonere786@gmail.com>
2023-08-07 23:34:52 +0530

    remoce deuplicate op

--- 02. Two Pointer/Easy/removeDuplicateFromSortedArray.cpp
+++ 02. Two Pointer/Easy/removeDuplicateFromSortedArray.cpp
@@ -15,6 +15,19 @@ int removeDuplicates(vector<int>& nums) {
         return s.size();
 }
 
+int removeDuplicatesOpt(vector<int>&nums){
+    int size = nums.size();
+        int i = 0;
+        int seen = INT_MIN;
+        for(int j=0; j<size; j++){
+           if(nums[j] != seen){
+               seen = nums[j];
+               nums[i++] = seen;
+           }
+        }
+        return i;
+}
+
 
 int main ()
 {
