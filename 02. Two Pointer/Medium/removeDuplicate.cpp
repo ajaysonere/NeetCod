92510c29689ea29c44fcd5b903c80ac3173f966d
Ajay Sonere <ajaysonere786@gmail.com>
2023-08-07 23:40:33 +0530

    remove duplicate

--- 02. Two Pointer/Medium/removeDuplicate.cpp
+++ 02. Two Pointer/Medium/removeDuplicate.cpp
@@ -1,7 +1,30 @@
 #include<iostream>
+#include<vector>
 using namespace std;
+
+int removeDuplicates(vector<int>& nums) {
+        int count = 1;
+        int i = 1;
+        while(i<nums.size()){
+            if(nums[i-1] == nums[i] && count < 2){
+                count++;
+                i++;
+            }else if(nums[i-1] == nums[i]){
+                nums.erase(nums.begin()+i);
+            }else if(nums[i-1]!= nums[i]){
+                count = 1;
+                i++;
+            }
+        }
+        return nums.size();
+}
 
 int main ()
 {
+    vector<int> nums = {1,1,1,2,2,3};
+    removeDuplicate(nums);
+    for(auto x : nums){
+        cout << x << " "; 
+    }
     return 0;
 }
