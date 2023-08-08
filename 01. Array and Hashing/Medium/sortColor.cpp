d88f80180046b5dd3c1a1fb2f81417e90c6cafad
Ajay Sonere <ajaysonere786@gmail.com>
2023-08-08 22:00:08 +0530

    sort color

--- 01. Array and Hashing/Medium/sortColor.cpp
+++ 01. Array and Hashing/Medium/sortColor.cpp
@@ -1,7 +1,41 @@
 #include<iostream>
+#include<vector>
+
 using namespace std;
+
+vector<int> sortColors(vector<int> &nums) {
+        // sort(nums.begin(),nums.end());
+        vector<int> ans(nums.size());
+        int index = 0;
+        for(int i=0; i<nums.size(); i++){
+            if(nums[i]==0){
+                ans[index] = nums[i];
+                index++;
+            }
+        }
+
+        for(int i=0; i<nums.size();i++){
+            if(nums[i] == 1){
+                ans[index] = nums[i];
+                index++;
+            }
+        }
+
+        for(int i=0;i<nums.size(); i++){
+            if(nums[i]==2){
+                ans[index++] = nums[i];
+            }
+        } 
+        return ans;
+}
+
 
 int main ()
 {
+    vector<int>nums = {2,0,2,1,1,0};
+    vector<int> ans = sortColors(nums);
+    for(auto x : ans){
+        cout << x << " ";
+    }
     return 0;
 }
