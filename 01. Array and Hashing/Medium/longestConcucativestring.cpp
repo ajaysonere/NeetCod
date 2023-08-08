aecb0f42d540fa86377108f9f9fda30cd80630d6
Ajay Sonere <ajaysonere786@gmail.com>
2023-08-08 22:06:51 +0530

    longest concucative string

--- 01. Array and Hashing/Medium/longestConcucativestring.cpp
+++ 01. Array and Hashing/Medium/longestConcucativestring.cpp
@@ -1,7 +1,35 @@
 #include<iostream>
+#include<vector>
+#include<set>
+#include<algorithm>
+
 using namespace std;
+
+int longestConsecutive(vector<int>& nums) {
+        if(nums.size() == 0) return 0;
+        sort(nums.begin() , nums.end());
+        int count = 1;
+        int longest = 1;
+        for(int i=0; i<nums.size()-1; i++){
+            if(nums[i] == nums[i+1]){
+                continue;
+            }
+            if(nums[i] == nums[i+1]-1){
+                count++;
+                if(count > longest){
+                    longest = count;
+                }
+            }else{
+                count = 1;
+            }
+        }
+        return longest;
+}
 
 int main ()
 {
+   vector<int> nums = {100,4,200,1,3,2};
+   int ans = longestConsecutive(nums);
+   cout << ans << "\n";
    return 0;
 }
