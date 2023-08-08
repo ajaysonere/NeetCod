6c2d751fc2f5ff9483e459b056ba53b01fdcee8e
Ajay Sonere <ajaysonere786@gmail.com>
2023-08-08 22:24:16 +0530

    sub array sum

--- 01. Array and Hashing/Medium/subArraySumOfK.cpp
+++ 01. Array and Hashing/Medium/subArraySumOfK.cpp
@@ -1,7 +1,31 @@
 #include<iostream>
+#include<vector>
+#include<unordered_map>
 using namespace std;
+
+int subarraySum(vector<int>& nums, int k) {
+        int size = nums.size();
+        int count = 0;
+        int sum = 0;
+        unordered_map<int,int>mp;
+        for(int i=0; i<size; i++){
+            sum += nums[i];
+            if(sum == k){
+                count++;
+            }
+            if(mp.find(sum-k) != mp.end()){
+                count += mp[sum-k];
+            }
+            mp[sum]++;
+        }
+        return count;
+}
 
 int main ()
 {
+    vector<int> nums = {1,1,1};
+    int k = 2;
+    int ans = subarraySum(nums , k);
+    cout << ans << "\n";
     return 0;
 }
