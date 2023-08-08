d78d9a4137db49bc1ed50adbe8c6c4cc0d80a032
Ajay Sonere <ajaysonere786@gmail.com>
2023-08-08 21:48:54 +0530

    remove duplicate

--- 02. Two Pointer/Medium/removeDuplicate.cpp
+++ 02. Two Pointer/Medium/removeDuplicate.cpp
@@ -22,7 +22,7 @@ int removeDuplicates(vector<int>& nums) {
 int main ()
 {
     vector<int> nums = {1,1,1,2,2,3};
-    removeDuplicate(nums);
+    removeDuplicates(nums);
     for(auto x : nums){
         cout << x << " "; 
     }
