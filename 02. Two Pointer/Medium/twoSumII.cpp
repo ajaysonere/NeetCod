1596251caba8d8a4a32225b21ce24aae1a87b363
Ajay Sonere <ajaysonere786@gmail.com>
2023-08-11 22:10:09 +0530

    two sum II

--- 02. Two Pointer/Medium/twoSumII.cpp
+++ 02. Two Pointer/Medium/twoSumII.cpp
@@ -1,22 +1,24 @@
 #include<iostream>
 using namespace std;
 
- int removeDuplicates(vector<int>& nums) {
-        int count = 1;
-        int i = 1;
-        while(i<nums.size()){
-            if(nums[i-1] == nums[i] && count < 2){
-                count++;
-                i++;
-            }else if(nums[i-1] == nums[i]){
-                nums.erase(nums.begin()+i);
-            }else if(nums[i-1]!= nums[i]){
-                count = 1;
-                i++;
+ vector<int> twoSum(vector<int>& nums, int target) {
+        vector<int> ans ;
+        int start = 0;
+        int end = nums.size()-1;
+        while(start < end){
+            if(nums[start]+nums[end] == target){
+                ans.push_back(start+1);
+                ans.push_back(end+1);
+                return ans;
+            }else if(nums[start]+nums[end] > target){
+                end--;
+            }else{
+                start++;
             }
         }
-        return nums.size();
+        return ans;
 }
+
 
 int main ()
 {
