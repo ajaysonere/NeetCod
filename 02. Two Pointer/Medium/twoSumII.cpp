296ff41aea65eb6fb27fc4fca4f976d564f8cf90
Ajay Sonere <ajaysonere786@gmail.com>
2023-08-11 22:02:50 +0530

    remove deuplicate II

--- 02. Two Pointer/Medium/twoSumII.cpp
+++ 02. Two Pointer/Medium/twoSumII.cpp
@@ -1,6 +1,30 @@
 #include<iostream>
 using namespace std;
 
+int removeDuplicates(vector<int> &nums)
+{
+    int count = 1;
+    int i = 1;
+    while (i < nums.size())
+    {
+        if (nums[i - 1] == nums[i] && count < 2)
+        {
+            count++;
+            i++;
+        }
+        else if (nums[i - 1] == nums[i])
+        {
+            nums.erase(nums.begin() + i);
+        }
+        else if (nums[i - 1] != nums[i])
+        {
+            count = 1;
+            i++;
+        }
+    }
+    return nums.size();
+}
+
 int main ()
 {
     return 0;
