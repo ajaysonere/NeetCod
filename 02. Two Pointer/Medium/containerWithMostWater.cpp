0b251f55711f96642a9e1553c2ad0801b4a4457c
Ajay Sonere <ajaysonere786@gmail.com>
2023-08-14 23:48:25 +0530

    container with most water solution

--- 02. Two Pointer/Medium/containerWithMostWater.cpp
+++ 02. Two Pointer/Medium/containerWithMostWater.cpp
@@ -1,7 +1,24 @@
-#include<iostream>
+#include<bits/stdc++.h>
+#include<vector>
 using namespace std;
+
+int maxArea(vector<int>& nums) {
+        int ans = INT_MIN;
+        int start = 0;
+        int end = nums.size()-1;
+        while(start < end){
+            int temp = min(nums[start] , nums[end]);
+            ans = max(ans , temp*(end-start));
+            if(nums[start] < nums[end]) start++;
+            else end--;
+        }
+        return ans;
+    }
 
 int main ()
 {
+    vector<int>nums = {1,8,6,2,5,4,8,3,7};
+    int ans = maxArea(nums);
+    cout << ans << "\n";
     return 0;
 }
